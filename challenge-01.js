8a9ff50736b3135f4dd8037af1247337435fec3a
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-03-31 20:39:36 -0300

    add challenge 1

---
+++ challenge-01.js
@@ -0,0 +1,22 @@
+// Write a function that takes an array of strings as argument
+// It should return the longest string
+
+function myFunction(vetor) {
+   let max = 0;
+   let maior;
+   for(let palavra of vetor) {
+      if(palavra.length > max) {
+         max = palavra.length;
+         maior = palavra
+      }
+   }
+   return maior;
+}
+
+myFunction(['I', 'need', 'candy'])
+
+// solution suggested
+
+function myFunction(input) {{
+        return input.reduce((a,b) => a.length <= b.length ? a : b)
+}
