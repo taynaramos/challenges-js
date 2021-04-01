6929a6a4ebe409c3cd11de70bc1fe295c67eb6f0
Tayná Martins Ramos <simples.tayna@gmail.com>
2021-03-31 22:32:48 -0300

    add challenge 6

---
+++ JS Basics/basic-06.js
@@ -0,0 +1,26 @@
+// Write a function that takes two numbers, say x and y, as arguments
+// Check if x is divisible by y
+// If yes, return x
+// If not, return the next higher natural number that is divisible by y
+
+function myFunction(x,y) {
+
+    if(x%y==0) {
+      return x
+    } else {
+      while(x%y !== 0){
+        x=x+1
+      }
+      return x
+    }
+  }
+  
+  myFunction(1, 23)
+  myFunction(-5, 7)
+
+  // suggested solution 
+
+  function myFunction2(x, y) {
+    while (x % y !== 0) x++;
+    return x;
+  }
